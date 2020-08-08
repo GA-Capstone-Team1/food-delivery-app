f45bfd91f12bda16ec8dd4766629c3ef482b9b2e
Omkar Kesarkhane <59394316+OmkarKesarkhane@users.noreply.github.com>
2020-08-08 20:51:04 +0530

    Revert "Pranay"

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,22 +1,2 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-import { api } from "../api";
-
-function Api() {
-  // let query = "categories";
-  useEffect(() => {
-    axios
-      .get(api + "location_details?entity_id=172835&entity_type=subzone", {
-        headers: {
-          Accept: "application/json",
-          "user-key": "dae550f2de39692413804f96c793bc96",
-        },
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  });
-  return <div>{/* <h1>THis is an api call</h1> */}</div>;
-}
-
-export default Api;
+export const api =
+  "https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/";
